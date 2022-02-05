d03931719303320d2ccfd9c526a069384eeb47ed
Peter Hindes <19994487+PeterHindes@users.noreply.github.com>
2022-02-05 14:00:16 -0700

    automate

---
+++ post.sh
@@ -0,0 +1,27 @@
+echo Please run ipfs daemon or setup the service
+set -o xtrace # Show the commands like bat files do
+# sudo pacman -S go-ipfs
+# ipfs init
+# get the key
+
+# Git no support hard links
+# symlinks dont work with ipfs as of writing
+if [ ! -f "publishlinked/index.html" ]; then # if not already linked
+ln index.html publishlinked/
+fi
+if [ ! -f "publishlinked/favicon.ico" ]; then # if not already linked
+ln favicon.ico publishlinked/
+fi
+
+s=$(bash -c "ipfs add -r publishlinked | grep -v /")
+ss=($s)
+sss=${ss[1]}
+echo $sss
+# ipfs daemon & # Does not work if already running
+ipfs name publish --key=GitKey $sss
+
+# Add to pinata for quick clouflare link
+. ./secrets.sh
+curl -X POST -H "pinata_api_key: $PIN_KEY; pinata_secret_api_key: $PIN_SEC" -d "{
+    hashToPin: $sss
+}"
